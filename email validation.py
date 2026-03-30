b031a43142de183a908afaac4978227da644e750
AMRITANSHU <amritanshuraj70@gmail.com>
2026-03-30 11:46:22 +0530

    Add files via upload

---
+++ email validation.py
@@ -0,0 +1,19 @@
+email = input("Enter your email address: ")
+if len(email) <=4:
+    if ("@" in email) and email.count("@") == 1:
+        pass
+    else:
+        print("Your email address is not valid 4")
+        if email[0]. isalpha ():
+            pass
+        else:
+            print("Your email address is not valid 3")
+            if email[0]. isdigit():
+                pass
+            else:
+                print("Your email address is not valid 2")
+
+
+
+    print("Your email address must have at least 4 characters 1")
+    exit()
